29e075e2c176227638212abd661855e5a11d08ad
madseason-live <53864518+madseason-live@users.noreply.github.com>
2019-08-07 18:37:47 +0200

    First one

---
+++ first.py
@@ -0,0 +1,13 @@
+from turtle import *
+for i in range(100):
+    forward(3.1416)
+    left(4.5)
+forward(200)
+for i in range(40):
+    forward(3.1416)
+    right(4.5)
+forward(200)
+for i in range(100):
+    forward(3.1416)
+    left(4.5)
+done()
